b2063052dbb5dfa225d0318fcc18ef8fa1568418
bejan festus <bejan@tangotech.co.in>
2024-02-10 20:09:33 +0530

    initial commit

---
+++ projects/tango-app-shared/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of tango-app-shared
+ */
+
+export * from './lib/tango-app-shared.service';
+export * from './lib/tango-app-shared.component';
+export * from './lib/tango-app-shared.module';
